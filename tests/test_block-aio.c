13d8f32dd97150c326f3f6ed74ae3aaa0db91a2f
Flavien Quesnel <flavien.quesnel@citrix.com>
2014-10-06 15:58:16 +0100

    CA-147672: [UnitTest] Add unit tests for tdaio_queue_write()
    Signed-off-by: Flavien Quesnel <flavien.quesnel@citrix.com>

--- tests/test_block-aio.c
+++ tests/test_block-aio.c
@@ -54,3 +54,42 @@ void test_tdaio_queue_read_computes_size_and_offset_correctly(void)
     // Call to the method to test
     tdaio_queue_read(&driver, treq);
 }
+
+void test_tdaio_queue_write_computes_size_and_offset_correctly(void)
+{
+    // Initialisation
+    td_driver_t driver;
+    td_request_t treq;
+
+    int expected_size;
+    uint64_t expected_offset;
+    struct aio_request aio;
+    struct tdaio_state prv;
+
+    driver.data = &prv;
+    treq.secs = 10;
+    driver.info.sector_size = 2048;
+    treq.sec = (uint64_t) 23;
+
+    prv.aio_free_count = 1;
+
+    prv.aio_free_list[0] = &aio;
+
+    // Expectations
+    expected_size = treq.secs * SECTOR_SIZE;
+    expected_offset = treq.sec * (uint64_t) SECTOR_SIZE;
+
+    td_prep_write_Expect(
+        &aio.tiocb,
+        prv.fd,
+        treq.buf,
+        expected_size,
+        expected_offset,
+        tdaio_complete,
+        &aio);
+
+    td_queue_tiocb_Ignore();
+
+    // Call to the method to test
+    tdaio_queue_write(&driver, treq);
+}
